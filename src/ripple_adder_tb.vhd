715de18acaafdbdc352f995060ded97f7ac02a42
robounicorn678 <145073376+robounicorn678@users.noreply.github.com>
2025-02-11 16:34:18 -0700

    here goes the first push, please work pt.2

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -50,11 +50,11 @@ begin
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
 	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"1" and w_Cout = '0') report "bad with ones" severity failure;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with zero & Cout" severity failure;
         w_addends <= x"60"; w_Cin <= '0'; wait for 10 ns;
-	       assert (w_sum = x"6" and w_Cout = '0') report "bad with ones" severity failure;
+	       assert (w_sum = x"6" and w_Cout = '0') report "bad with 6 & 0" severity failure;
 		w_addends <= x"90"; w_Cin <= '0'; wait for 10 ns;
-	       assert (w_sum = x"9" and w_Cout = '1') report "bad with ones" severity failure;
+	       assert (w_sum = x"9" and w_Cout = '1') report "bad with 9 & 0" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
